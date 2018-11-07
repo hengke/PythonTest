0fb47e4318b822bbedf93220eb2d9ebbcccfe1ea
hengke <zhenghengke@qq.com>
2018-11-07 17:30:12 +0800

    默认更改列表

--- Json/下载定向井基础数据.py
+++ Json/下载定向井基础数据.py
@@ -28,37 +28,40 @@ if __name__ == "__main__":
         "page": 1,
         "rows": 100
     }
-    JsonStr = requests.post(
-        DillBaseData_Url, headers=headers, data=data, cookies=cookies)
+    JsonStr = requests.post(DillBaseData_Url, headers=headers, data=data, cookies=cookies)
     result = JsonStr.json()
-    print("Page %d/%d" % (data["page"], result['result']['totalPages']))
-    if result["success"] is True:
-        bb = result
-        while bb["result"]["lastPage"] is False:
-            data["page"] += 1
-            JsonStr = requests.post(
-                DillBaseData_Url, headers=headers, data=data, cookies=cookies)
-            bb = JsonStr.json()
-            print(
-                "Page %d/%d" % (data["page"], result['result']['totalPages']))
-            if bb["success"] is True:
-                result["result"]["content"] += bb["result"]["content"]
-            pass
-    content = result["result"]["content"]
-    file = open(JsonFileName, "w", 1)
-    # 6、写入文件
-    file.write(str(result))
-    # 7、关闭文件
-    file.close()
-    print("下载完成！")
-    # open函数中encoding参数设为'utf-8'时，文件以'utf-8'编码格式保存文件，Excel打开csv文件中文乱码
-    # encoding设为'utf_8_sig'，文件以'utf-8-bom'编码格式保存文件，Excel打开csv文件中文正常显示
-    # encoding设为'gb18030'，文件以'gb18030'编码格式保存文件，Excel打开csv文件中文正常显示
-    with open(CsvFileName, 'w', newline='', encoding='gb18030') as f:
-        writer = csv.writer(f)
-        writer.writerow(content[0].keys())
-        for row in content:
-            # print(row.values())
-            writer.writerow(row.values())
-    f.close()
-    print("转换完成")
+    if result['success'] is False:
+        print("错误：", result['result'])
+    else:
+        print("Page %d/%d" % (data["page"], result['result']['totalPages']))
+        if result["success"] is True:
+            bb = result
+            while bb["result"]["lastPage"] is False:
+                data["page"] += 1
+                JsonStr = requests.post(
+                    DillBaseData_Url, headers=headers, data=data, cookies=cookies)
+                bb = JsonStr.json()
+                print(
+                    "Page %d/%d" % (data["page"], result['result']['totalPages']))
+                if bb["success"] is True:
+                    result["result"]["content"] += bb["result"]["content"]
+                pass
+        content = result["result"]["content"]
+        file = open(JsonFileName, "w", 1)
+        # 6、写入文件
+        file.write(str(result))
+        # 7、关闭文件
+        file.close()
+        print("下载完成！")
+        # open函数中encoding参数设为'utf-8'时，文件以'utf-8'编码格式保存文件，Excel打开csv文件中文乱码
+        # encoding设为'utf_8_sig'，文件以'utf-8-bom'编码格式保存文件，Excel打开csv文件中文正常显示
+        # encoding设为'gb18030'，文件以'gb18030'编码格式保存文件，Excel打开csv文件中文正常显示
+        with open(CsvFileName, 'w', newline='', encoding='gb18030') as f:
+            writer = csv.writer(f)
+            writer.writerow(content[0].keys())
+            for row in content:
+                # print(row.values())
+                writer.writerow(row.values())
+        f.close()
+        print("转换完成")
+        pass
