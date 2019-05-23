eba589ef708c312b052ab972189425c292cfd6b3
hengke <zhenghengke@qq.com>
2019-05-23 17:17:28 +0800

    更改为函数

--- 爬虫/爬虫-消防工程师网课.py
+++ 爬虫/爬虫-消防工程师网课.py
@@ -4,31 +4,41 @@ import json
 # import ssl
 # import os
 
-if __name__ == "__main__":
-    url_VideoBasic = "https://www.zongtongedu.com/video/VideoBasic"
+def GetCourseVideoUrl(headers, CourseInfo, basicInfoData, VideoBasicData)
     url_basicInfo = "https://www.zongtongedu.com/video/basicInfo"
-    url_basicList = "https://www.zongtongedu.com/Video/basicList"
-    # cookies = {
-    #     "ASP.NET_SessionId": "4qtjjehksqmxx1l4xolrxd5x",
-    #     "p_h5_u":"14CE65DC-11D8-4374-83E2-0964D51A19C9",
-    #     "selectedStreamLevel": "SD",
-    #     "uToKen":"480a644e33feea6dcff6685eb2eee7ef"
-    # }
+    url_VideoBasic = "https://www.zongtongedu.com/video/VideoBasic"
 
-    # 教材精讲班
-    data = {
-        "examid":12,
-        "courseid":3,
-        "vtfid":5
-    }
-    session = requests.session()
-    LogInData = {
-        "PhoneText":"13111469643",
-        "PasswordText":"659061",
-        "chkRem":"checked"
-    }
-    session.post("https://www.zongtongedu.com/Login/login",data = LogInData)
-    userDetails = "https://www.zongtongedu.com/uc/userDetails"
+    JsonStr = session.post(url_basicInfo, headers=headers, data=basicInfoData)
+    basicInfo = JsonStr.json()
+
+    Chapters = basicInfo['Data']['infoList']
+    for Chapter in Chapters:
+        Sections = Chapter["infoList"]
+        for Section in Sections:
+            data = {
+                "examid": 12,
+                "courseid": Section["courseid"],
+                "vid": Section["vid"]
+            }
+            JsonStr = session.post(url_VideoBasic, headers=headers, data=VideoBasicData)
+            resultVideo = JsonStr.json()
+            Title = Chapter["title"] + " " + Section["title"]
+            VideoInfo = {
+                "filename": Title.replace(" ", "_") + ".mp4",
+                "url": resultVideo['Data']['vUrl']
+            }
+            ClassInfo["videolist"].append(VideoInfo)
+            print(VideoInfo)
+    # JsonFileName = r"F:\消防工程师\2018\教材精讲班_技术综合能力.json"
+    JsonFileName = r"C:\教材精讲班_技术综合能力.json"
+    with open(JsonFileName, 'w') as f:
+        json.dump(ClassInfo, f, ensure_ascii=False, indent=0)
+
+
+if __name__ == "__main__":
+
+    url_basicList = "https://www.zongtongedu.com/Video/basicList"
+    url_userDetails = "https://www.zongtongedu.com/uc/userDetails"
     # headers = {
     #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
     #     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -46,36 +56,41 @@ if __name__ == "__main__":
         "X-Requested-With": "XMLHttpRequest",
         "Connection": "keep-alive",
     }
-    # JsonStr = session.post(url_basicInfo, headers=headers, data=data, cookies=cookies)
-    JsonStr = session.post(url_basicInfo, headers=headers, data=data)
-    result = JsonStr.json()
 
-    ClassInfo =  {
+    with open(r'F:\消防工程师\LogInData.json', "r") as f:
+        LogInData = json.load(f)
+
+    session = requests.session()
+
+    url_Login = "https://www.zongtongedu.com/Login/login"
+    session.post(url_Login,data = LogInData)
+
+    data = {
+        "examid":12,
+        "year":0
+    }
+    url_firstBasic = "https://www.zongtongedu.com/video/firstBasic"
+    JsonStr = session.post(url_firstBasic, headers=headers, data=data)
+    firstBasic = JsonStr.json()
+    with open(r'C:\firstBasic.json', 'w') as f:
+        json.dump(firstBasic, f, ensure_ascii=False, indent=0)
+
+    url_GetCouse = "https://www.zongtongedu.com/video/GetCouse"
+    JsonStr = session.post(url_firstBasic, headers=headers, data={"examid":12})
+    GetCouses = JsonStr.json()
+    with open(r'C:\GetCouses.json', 'w') as f:
+        json.dump(GetCouses, f, ensure_ascii=False, indent=0)
+
+    CourseInfo = {
         "vtfid":5,
         "vtYear": 2018,
         "vtTitle": "教材精讲班",
         "classname": "技术综合能力",
         "videolist": []
     }
-    Chapters = result['Data']['infoList']
-    for Chapter in Chapters:
-        Sections = Chapter["infoList"]
-        for Section in Sections:
-            data = {
-                "examid": 12,
-                "courseid": Section["courseid"],
-                "vid": Section["vid"]
-            }
-            JsonStr = requests.post(url_VideoBasic, headers=headers, data=data, cookies=cookies)
-            resultVideo = JsonStr.json()
-            Title = Chapter["title"] + " " + Section["title"]
-            VideoInfo = {
-                "filename": Title.replace(" ", "_") + ".mp4",
-                "url": resultVideo['Data']['vUrl']
-            }
-            ClassInfo["videolist"].append(VideoInfo)
-            print(VideoInfo)
-    # JsonFileName = r"F:\消防工程师\2018\教材精讲班_技术综合能力.json"
-    JsonFileName = r"C:\教材精讲班_技术综合能力.json"
-    with open(JsonFileName, 'w') as f:
-        json.dump(ClassInfo, f, ensure_ascii=False, indent=0)
+    data = {
+        "examid": 12,
+        "courseid": 3,
+        "vtfid": 5
+    }
+
